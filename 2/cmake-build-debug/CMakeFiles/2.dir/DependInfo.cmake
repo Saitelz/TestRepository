931ad477640b29d6b319b024c7bbb9aa7c684a47
Saitelz <saitelz@yandex.ru>
2021-09-27 00:03:08 +0300

    update 2 project

--- 2/cmake-build-debug/CMakeFiles/2.dir/DependInfo.cmake
+++ 2/cmake-build-debug/CMakeFiles/2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/pavelmaksimov/Desktop/CLionProjects/TestProjectForGit/2/main.cpp" "/Users/pavelmaksimov/Desktop/CLionProjects/TestProjectForGit/2/cmake-build-debug/CMakeFiles/2.dir/main.cpp.o"
+  "/Users/pavelmaksimov/Desktop/CLionProjects/TestProjectForGit/TestRepository/2/main.cpp" "/Users/pavelmaksimov/Desktop/CLionProjects/TestProjectForGit/TestRepository/2/cmake-build-debug/CMakeFiles/2.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- 2/main.cpp
+++ 2/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World =)!" << std::endl;
+    std::cout << "Hello, World =) Yo!" << std::endl;
     return 0;
 }
